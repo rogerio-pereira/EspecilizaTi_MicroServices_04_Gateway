e0124338200e40b9110d12594556349db3060d83
Rogerio Pereira <hi@rogeriopereira.dev>
2021-08-06 18:27:53 -0400

    Aula: 02 - Listar as Empresas Laravel Micro Gateway

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
     return response()->json(['message' => 'success']);
+});
+
+Route::get('companies', function(){
+    $response = Http::withHeaders(['Accept' => 'application/json'])
+                    ->get(config('microservice.available.micro_01.url').'/companies');
+    $return = json_decode($response->body());
+
+    return response()->json($return, $response->status());
 });
