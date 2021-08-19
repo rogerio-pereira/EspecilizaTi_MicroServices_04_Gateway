36653a0bff5a1576b9dac3f5e88f29909182fefa
Rogerio Pereira <hi@rogeriopereira.dev>
2021-08-19 19:37:00 -0400

    Aula: 04 - Criar Service Layer Laravel Micro Gateway

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,12 @@
 <?php
 
-use App\Http\Utils\DefaultResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\{
+    CompanyController
+};
 
 Route::get('/', function(){
     return response()->json(['message' => 'success']);
 });
 
-Route::get('companies', function(DefaultResponse $defaultResponse, Request $request){
-    $response = Http::withHeaders([
-                        'Accept' => 'application/json'
-                    ])
-                    ->get(
-                        config('microservice.available.micro_01.url').'/companies',
-                        $request->all()
-                    );
-
-    return $defaultResponse->response($response);
-});
+Route::get('companies', [CompanyController::class, 'index']);
